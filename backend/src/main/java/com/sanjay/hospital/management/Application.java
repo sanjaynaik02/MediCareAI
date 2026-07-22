57ff7b1d3fd1e1d0bf01f232cc2daf51ee304ed4
Sanjay Naik M <snjy600@gmail.com>
2026-07-22 23:14:27 +0530

    Backend Foundation & Environment Setup

---
+++ backend/src/main/java/com/sanjay/hospital/management/Application.java
@@ -0,0 +1,13 @@
+package com.sanjay.hospital.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Annotations
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
